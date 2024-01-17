357d6102920184b136f92e37bca711c84a4fe60e
Lucas Saliés Brum <sistematico@gmail.com>
2024-01-17 06:36:13 -0400

    Update

--- apps/site/src/main.ts
+++ apps/site/src/main.ts
@@ -1,11 +1,23 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from '@/routes'
-import '@/assets/scss/tailwind.scss'
-import App from '@/app.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "@/routes";
+import "@/assets/scss/tailwind.scss";
+import App from "@/app.vue";
+// import { store } from "@/store"
 
-const router = createRouter({ history: createWebHistory(), routes })
-const app = createApp(App)
+const router = createRouter({ history: createWebHistory(), routes });
 
-app.use(router)
-app.mount('#app')
+router.beforeEach((_to, _from, next) => {
+  const host = window.location.host;
+  const subdomain = host.split('.')[1] ? host.split('.')[0] : false;
+
+  // if 
+
+  console.log("Host:", host, "Sub-domain:", subdomain);
+  next();
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.mount("#app");
