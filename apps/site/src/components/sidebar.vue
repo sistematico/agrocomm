5d3409965caa064d81f86a4fc64f7df17b03efb9
Lucas Saliés Brum <sistematico@gmail.com>
2024-04-05 14:15:16 -0400

    Update layout

--- apps/site/src/components/sidebar.vue
+++ apps/site/src/components/sidebar.vue
@@ -4,7 +4,7 @@ defineProps({
 })
 </script>
 <template>
-  <div class="card mt-2 mt-md-0" v-for="item in json">
+  <div class="card mt-2 mt-md-0 mb-2" v-for="item in json">
     <div class="card-header fs-4">{{ item.title }}</div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item" v-for="link in item.links">
