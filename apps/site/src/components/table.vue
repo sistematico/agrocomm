550fdeae383fe822b208f52a335c2424d4fec2b5
Lucas Saliés Brum <sistematico@gmail.com>
2024-04-24 15:48:01 -0400

    Update

--- apps/site/src/components/table.vue
+++ apps/site/src/components/table.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useState } from '@/composables/states'
+
 defineProps({
   data: { type: Object, required: true }
 })
@@ -7,7 +9,7 @@ defineProps({
   <table class="table table-hover">
     <thead>
       <tr>
-        <th scope="col" v-for="(value, key) in data[0]" :key="key">
+        <th scope="col" v-for="(_, key) in data[0]" :key="key">
           {{ key }}
         </th>
       </tr>
@@ -15,9 +17,66 @@ defineProps({
     <tbody>
       <tr v-for="item in data" :key="item.id">
         <td v-for="(value, key) in item" :key="key">
-          {{ value }}
+          <!-- <div class="input-group">
+            <div class="input-group-text" id="btnGroupAddon">R$</div>
+            <input type="text" class="form-control" placeholder="Input group example" aria-label="Input group example" aria-describedby="btnGroupAddon">
+          </div> -->
+
+          <template v-if="String(key) === 'Preco'">
+            <div class="btn-group" role="group" aria-label="Price Group">
+              <button type="button" class="btn btn-sm btn-outline-green" disabled>R$</button>
+              <button type="button" class="btn btn-sm btn-green" disabled>{{ value }}</button>
+            </div>
+          </template>
+          <template v-else-if="String(key) === 'Estado'">
+            <div class="btn-group" role="group" aria-label="Price Group">
+              <button type="button" class="btn btn-sm btn-outline-green">
+                <img width="20" height="20" :src="`https://cdn.agrocomm.com.br/images/bandeiras/square-rounded/${value.toLowerCase()}.svg`" :alt="value" /> 
+              </button>
+              <button type="button" class="btn btn-sm btn-green">{{ useState(value) }}</button>
+            </div>
+          </template>
+          <template v-else>
+            {{ value }}
+          </template>
         </td>
       </tr>
     </tbody>
   </table>
 </template>
+<style scoped>
+.btn-sm, .btn-group-sm > .btn {
+  padding: 0.15rem 0.35rem;
+  font-size: 0.80rem;
+}
+
+#first{
+  opacity: 1;
+  transition: opacity 1s;
+}
+
+#first:visited{
+  opacity: 0;
+}
+
+#first:focus{
+  opacity: 0;
+}
+
+#second:focus{
+  outline:none
+}
+
+#second{
+  opacity: 0;
+  transition: opacity 1s;
+}
+
+#first:focus + #second{
+  opacity: 1;
+}
+
+#first:visited ~ #second{
+  opacity: 1;
+}  
+</style>
---
+++ apps/site/src/composables/states.ts
@@ -0,0 +1,43 @@
+type StateMap = {
+  [key: string]: string
+}
+
+const states: StateMap[] = [
+  { 'AC': 'Acre' },
+  { 'AL': 'Alagoas' },
+  { 'AP': 'Amapá' },
+  { 'AM': 'Amazonas' },
+  { 'BA': 'Bahia' },
+  { 'CE': 'Ceará' },
+  { 'DF': 'Distrito Federal' },
+  { 'ES': 'Espírito Santo' },
+  { 'GO': 'Goiás' },
+  { 'MA': 'Maranhão' },
+  { 'MT': 'Mato Grosso' },
+  { 'MS': 'Mato Grosso do Sul' },
+  { 'MG': 'Minas Gerais' },
+  { 'PA': 'Pará' },
+  { 'PB': 'Paraíba' },
+  { 'PR': 'Paraná' },
+  { 'PE': 'Pernambuco' },
+  { 'PI': 'Piauí' },
+  { 'RJ': 'Rio de Janeiro' },
+  { 'RN': 'Rio Grande do Norte' },
+  { 'RS': 'Rio Grande do Sul' },
+  { 'RO': 'Rondônia' },
+  { 'RR': 'Roraima' },
+  { 'SC': 'Santa Catarina' },
+  { 'SP': 'São Paulo' },
+  { 'SE': 'Sergipe' },
+  { 'TO': 'Tocantins' }
+]
+
+// export function useState(state: string) {
+//   return states.find(s => s[state])
+//   // return states[state]
+// }
+
+export function useState(stateAbbreviation: string): string {
+  const stateEntry = states.find(stateMap => stateMap[stateAbbreviation]);
+  return stateEntry ? stateEntry[stateAbbreviation] : '';
+}
