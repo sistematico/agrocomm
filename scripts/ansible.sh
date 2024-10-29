c8c7a69aea9c671397ba82c12ba5263954e52eb0
Lucas Saliés Brum <sistematico@gmail.com>
2024-10-28 21:39:20 -0400

    Added error pages

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ansible-playbook ./ansible/playbook.yml -i eris.paxa.dev,
+if [ -f /etc/arch-release ]; then
+  ansible-playbook -e "ansible_port=2200" /home/lucas/code/agrocomm/ansible/playbook.yml -i eris.paxa.dev,
+else
+  # ansible-playbook --connection=local ./ansible/playbook.yml -i 127.0.0.1,
+  ansible-playbook --connection=local -e "ansible_port=2200" /var/www/agrocomm/ansible/playbook.yml -i localhost,
+fi
