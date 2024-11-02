706a413050b19e5ad67d0b615cfc4cbd9884d42f
Lucas Saliés Brum <sistematico@gmail.com>
2024-11-02 09:39:58 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,18 +5,18 @@ PATH=$PATH:/home/nginx/.bun/bin
 git clean -fxd
 
 cd ./apps/api
-rm -rf ./src/db/meta/ ./src/db/*.sql *.db
-cp .env.production .env
+#rm -rf ./src/db/meta/ ./src/db/*.sql *.db
+#cp .env.production .env
 bun install
 
-bun run db:push
-bun run db:generate
-bun run db:seed
+#bun run db:push
+#bun run db:generate
+#bun run db:seed
 
 sudo /usr/bin/systemctl restart agrocomm-hono.service
 
 cd ../site
 # [ ! -f .env ] && cp .env.production .env
-cp .env.production .env
+#cp .env.production .env
 bun install
 bun run build
