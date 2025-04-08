7fd558edfd7018ed395b1996f4f2e8f47195815f
Lucas Saliés Brum <sistematico@gmail.com>
2025-04-08 12:36:03 -0400

    Update

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -1,25 +1,25 @@
-import { db } from '@/db'
-import { users } from '@/db/schema'
-import { SignupFormSchema, FormState } from '@/app/lib/definitions'
+// import { db } from '@/db'
+// import { users } from '@/db/schema'
+// import { SignupFormSchema, FormState } from '@/app/lib/definitions'
 // import { cookies } from 'next/headers'
 // import { deleteSession } from '@/app/lib/session'
 // import { redirect } from 'next/navigation'
  
-export async function signup(state: FormState, formData: FormData) {
-  const validatedFields = SignupFormSchema.safeParse({
-    name: formData.get('name'),
-    email: formData.get('email'),
-    password: formData.get('password'),
-  })
+// export async function signup(state: FormState, formData: FormData) {
+  // const validatedFields = SignupFormSchema.safeParse({
+  //   name: formData.get('name'),
+  //   email: formData.get('email'),
+  //   password: formData.get('password'),
+  // })
  
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-    }
-  }
+  // if (!validatedFields.success) {
+  //   return {
+  //     errors: validatedFields.error.flatten().fieldErrors,
+  //   }
+  // }
  
   // Call the provider or db to create a user...
-}
+// }
  
 // export async function logout() {
 //   deleteSession()
