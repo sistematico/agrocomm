b05ab640461b445fd3c9bb9f91ef44f38810bdae
Lucas Saliés Brum <sistematico@gmail.com>
2025-07-11 16:17:11 -0400

    Updates

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,7 +2,6 @@ import { db } from '@/db'
 import { users } from '@/db/schema'
 import { hashPassword, generateSalt } from '@/app/lib/password'
 
-// const password = await Bun.password.hash('agrocomm')
 const password = await hashPassword('agrocomm', generateSalt())
 
 async function main() {
